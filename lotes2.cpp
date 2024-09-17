5b2a392ff64088f4d38a82caaf7a03815b9e888e
ramirez0801 <111304836+ramirez0801@users.noreply.github.com>
2024-09-17 10:15:01 -0600

    Update lotes2

--- lotes2.cpp
+++ lotes2.cpp
@@ -5,19 +5,22 @@
 #include <thread>
 #include <chrono>
 #include <conio.h>
+#include <queue>
+
+using namespace std;
 
 class Proceso {
 
 public:
     int id;
     int tiempo_max;
+    int tiempo_rest;
     char operacion;
     int operandos[2];
-    std::string resultado;
-    std::string estado;
+    string resultado;
+    string estado;
 
-    Proceso(int id, int tiempo_max, char operacion, int op1, int op2)
-        : id(id), tiempo_max(tiempo_max), operacion(operacion) {
+    Proceso(int id, int tiempo_max, char operacion, int op1, int op2): id(id), tiempo_max(tiempo_max), operacion(operacion) {
         operandos[0] = op1;
         operandos[1] = op2;
         estado = "En espera";
@@ -27,28 +30,28 @@ public:
         try {
             switch (operacion) {
                 case '+':
-                    resultado = std::to_string(operandos[0] + operandos[1]);
+                    resultado = to_string(operandos[0] + operandos[1]);
                     break;
                 case '-':
-                    resultado = std::to_string(operandos[0] - operandos[1]);
+                    resultado = to_string(operandos[0] - operandos[1]);
                     break;
                 case '*':
-                    resultado = std::to_string(operandos[0] * operandos[1]);
+                    resultado = to_string(operandos[0] * operandos[1]);
                     break;
                 case '/':
                     if (operandos[1] != 0)
-                        resultado = std::to_string(operandos[0] / operandos[1]);
+                        resultado = to_string(operandos[0] / operandos[1]);
                     else
-                        throw std::runtime_error("División por cero");
+                        throw runtime_error("División por cero");
                     break;
                 case '%':
-                    resultado = std::to_string(operandos[0] % operandos[1]);
+                    resultado = to_string(operandos[0] % operandos[1]);
                     break;
                 default:
-                    throw std::runtime_error("Operación desconocida");
+                    throw runtime_error("Operación desconocida");
             }
             estado = "Terminado";
-        } catch (const std::exception& e) {
+        } catch (const exception& e) {
             resultado = "Error";
             estado = "Error";
         }
@@ -59,7 +62,8 @@ std::vector<Proceso> generar_procesos(int num_procesos) {
     std::vector<Proceso> procesos;
     char operaciones[] = {'+', '-', '*', '/', '%'};
     std::srand(std::time(0));
-    for (int i = 0; i < num_procesos; ++i) {
+    for (int i = 0; i < num_procesos; ++i)
+    {
         int id = i + 1;
         int tiempo_max = std::rand() % 16 + 5;
         char operacion = operaciones[std::rand() % 5];
@@ -70,34 +74,7 @@ std::vector<Proceso> generar_procesos(int num_procesos) {
     return procesos;
 }
 
-void ejecutar_procesos(std::vector<Proceso>& procesos) {
-    char comando;
-    for (auto& proceso : procesos) {
-        proceso.estado = "En ejecución";
-        std::cout << "Ejecutando proceso " << proceso.id << "...\n";
-        std::this_thread::sleep_for(std::chrono::milliseconds(proceso.tiempo_max * 100));
-        proceso.ejecutar();
-        std::cout << "Proceso " << proceso.id << " terminado con resultado: " << proceso.resultado << "\n";
-        
-        if(kbhit())
-        {
-            std::cout << "Presiona 'I' para interrupción, 'E' para error, 'P' para pausa, 'C' para continuar: ";
-            std::cin >> comando;
-            comando = std::toupper(comando);
-            if (comando == 'I') {
-                interrupcion(procesos);
-            } else if (comando == 'E') {
-                error(procesos);
-            } else if (comando == 'P') {
-                pausa();
-            } else if (comando == 'C') {
-                continue;
-            }
-        }
-    }
-}
-
-void interrupcion(std::vector<Proceso>& procesos) {
+void interrupcion(std::vector<Proceso>& procesos, queue& interr) {
     if (!procesos.empty()) {
         Proceso proceso = procesos.front();
         procesos.erase(procesos.begin());
@@ -126,18 +103,46 @@ void pausa() {
     }
 }
 
+void ejecutar_procesos(std::vector<Proceso>& procesos, queue& interr, queue& error, queue& termi) {
+    char comando;
+    for (auto& proceso : procesos) {
+        proceso.estado = "En ejecución";
+        std::cout << "Ejecutando proceso " << proceso.id << "...\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(proceso.tiempo_max * 100));
+        proceso.ejecutar();
+        //Mostrar tablas
+        //barra de progreso
+            if(kbhit())
+            {
+                std::cout << "Presiona 'I' para interrupción, 'E' para error, 'P' para pausa, 'C' para continuar: ";
+                std::cin >> comando;
+                comando = std::toupper(comando);
+                if (comando == 'I') {
+                    interrupcion(procesos, interr);
+                } else if (comando == 'E') {
+                    error(procesos);
+                } else if (comando == 'P') {
+                    pausa();
+                } else if (comando == 'C') {
+                    continue;
+                }
+            }
+
+        std::cout << "Proceso " << proceso.id << " terminado con resultado: " << proceso.resultado << "\n";
+    }
+}
+
 int main() {
     int num_procesos;
+    queue interr, error, termi;
     std::cout << "Número de proceos inicial: ";
     std::cin >> num_procesos;
 
     std::vector<Proceso> procesos = generar_procesos(num_procesos);
 
-    while (!procesos.empty()) {
-        ejecutar_procesos(procesos);
-        
-        
-        
+    while (!procesos.empty())
+    {
+        ejecutar_procesos(procesos, interr, error, termi);
     }
 
     return 0;
